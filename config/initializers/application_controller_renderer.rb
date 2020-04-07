05aab5cd78f14742f7d4cce882bbf34b200356ed
TAKERU ARAKAWA <takeruarakawa@TAKERUnoMacBook-Air.local>
2020-04-07 23:11:18 +0900

    [Finish]3-9

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '402f2bcfc7fd8dd07bded7b2f9d53162e532b3159ac7ad8aa64453cde1168fa16a61af965e4039f03b63c0d5f9a9d377f2587bf8fddc7f77611b5f0011bba135'
